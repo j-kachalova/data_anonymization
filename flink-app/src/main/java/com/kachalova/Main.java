81ccf43750101ab6866f6b771912abe6e86342dc
j-kachalova <62459071+j-kachalova@users.noreply.github.com>
2025-05-04 22:56:56 +0400

    add fat jar in pom.xml

--- flink-app/src/main/java/com/kachalova/Main.java
+++ flink-app/src/main/java/com/kachalova/Main.java
@@ -1,7 +1,27 @@
 package com.kachalova;
 
+import com.kachalova.deserializer.JSONValueDeserializationSchema;
+import com.kachalova.dto.RequestDto;
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.connector.kafka.source.KafkaSource;
+import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws Exception {
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        String topic = "make-anonymous";
+        KafkaSource<RequestDto> source = KafkaSource.<RequestDto>builder()
+                .setBootstrapServers("localhost:9092")
+                .setTopics(topic)
+                .setGroupId("myGroup")
+                .setStartingOffsets(OffsetsInitializer.earliest())
+                .setValueOnlyDeserializer(new JSONValueDeserializationSchema())
+                .build();
+        DataStream<RequestDto> transactionStream = env.fromSource(source, WatermarkStrategy.noWatermarks(), "Kafka source");
+
+        transactionStream.print();
+        env.execute("Flink Ecommerce Realtime Streaming");
     }
 }
---
+++ flink-app/src/main/java/com/kachalova/dto/RequestDto.java
@@ -0,0 +1,12 @@
+package com.kachalova.dto;
+
+
+
+public class RequestDto {
+    String message;
+
+    public RequestDto(String message) {
+        this.message = message;
+    }
+
+}
